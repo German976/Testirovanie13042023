76188f32028942bfaa12059039e3802210a40c04
German976 <112702327+German976@users.noreply.github.com>
2023-04-13 13:23:21 +0300

    content

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testirovanie13042023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+
+            if(a == 1 || a == 2 || a == 12)
+            {
+                Console.WriteLine("Зима");
+            }
+            else if(a == 3 || a == 4 || a == 5) 
+            {
+                Console.WriteLine("Весна");
+            }
+            else if (a == 6 || a == 7 || a == 8)
+            {
+                Console.WriteLine("Лето");
+            }
+            else if (a == 9 || a == 10 || a == 11)
+            {
+                Console.WriteLine("Осень");
+            }
+
+        }
+    }
+}
